f0eb082c767e0aae1e2ced867b802ed68e6a39a5
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2015-12-15 21:05:35 -0500

    Added function for deallocation and testing

--- disk.cpp
+++ disk.cpp
@@ -24,7 +24,7 @@ Disk::Disk(int n, int size) {
 // printSpace - print the disk system as it is
 void Disk::printSpace() {
 
-    std::cout << "[process " << pthread_self() << "] Simulated Disk Space Allocation:" << std::endl;
+    std::cout << "[thread " << pthread_self() << "] Simulated Disk Space Allocation:" << std::endl;
     std::cout << std::string(n_blocks / 4, '=');
 
     std::map<int, DiskCluster>::const_iterator itr = diskSpace.begin();
@@ -46,25 +46,137 @@ void Disk::printSpace() {
 }
 
 // allocFile - returns the int for the first cluster allocated for a file
-int Disk::allocFile(const std::string& name, int num_blocks) {
-    // do nothing for now except print the disk system
+int Disk::allocFile(const std::string& name, int num_bytes) {
 
-    std::map<std::string, int>::iterator itr = fileMap.find(name);
-    if (itr != fileMap.end()) {
+    std::map<std::string, int>::iterator fileItr = fileMap.find(name);
+    if (fileItr != fileMap.end()) {
         // file already exists, return with error
         return -1;
     }
 
-    // figure out the character to print and allocate space for it
-    
-    printSpace();
+    // allocate space as necessary
+    int num_blocks = num_bytes / blocksize;
+    if (num_bytes % blocksize != 0) {
+        num_blocks++;
+    }
 
-    return 0;
+    std::map<int, DiskCluster>::iterator itr = diskSpace.begin();
+    int first_cluster = -1;
+    int last_cluster = -1;
+    int num_clusters = 0;
+
+    while (itr != diskSpace.end()) {
+        if(itr->second.getDisplayChar() == '.' && num_blocks > 0) {
+            // if the remainder of the file fits, split the cluster
+            if (itr->second.getSize() >= num_blocks) {
+                int next_size = itr->second.getSize() - num_blocks;
+
+                DiskCluster newAlloc (num_blocks, name, nextFileChar);
+                diskSpace[itr->first] = newAlloc;
+
+                DiskCluster nextAlloc (next_size, "", '.');
+                diskSpace[itr->first + num_blocks] = nextAlloc;
+
+                num_blocks = 0;
+            }
+            // otherwise, use this cluster and keep looking for more space
+            else {
+                DiskCluster newAlloc (itr->second.getSize(), name, nextFileChar);
+                diskSpace[itr->first] = newAlloc;
+
+                num_blocks -= itr->second.getSize();
+            }
+
+            num_clusters++;
+
+            if (first_cluster < 0) {
+                first_cluster = itr->first;
+                fileMap[name] = itr->first;
+            }
+            if (last_cluster > 0) {
+                diskSpace[last_cluster].setNextCluster(itr->first);
+            }
+            last_cluster = itr->first;
+        }
+        itr++;
+    }
+
+    // advance the character if we successfully allocated
+    if (first_cluster >= 0) {
+        int total_blocks = (num_bytes / blocksize);
+        if (num_bytes % blocksize != 0)
+            total_blocks++;
+
+        std::cout << "[thread " << pthread_self() << "] Stored file '";
+        std::cout << nextFileChar;
+
+        if (num_bytes == 1)
+            std::cout << "' (" << num_bytes << " byte; ";
+        else
+            std::cout << "' (" << num_bytes << " bytes; ";
+
+        if (total_blocks == 1)
+            std::cout << total_blocks << " block; ";
+        else
+            std::cout << total_blocks << " blocks; ";
+        
+        if (num_clusters == 1)
+            std::cout << num_clusters << " cluster)" << std::endl;
+        else
+            std::cout << num_clusters << " clusters)" << std::endl;
+
+        nextFileChar++;
+        printSpace();
+    }
+    else {
+        // TODO: print an error message or something?
+    }
+
+    return first_cluster;
 }
 
 // deallocFile - deallocates disk space for a file and deletes
 void Disk::deallocFile(const std::string& name) {
-    // do nothing for now except print disk system
+
+    if (fileMap.find(name) == fileMap.end()) {
+        // TODO: print an error message
+        return;
+    }
+
+    int index = fileMap.find(name)->second;
+
+    std::map<int, DiskCluster>::iterator itr = diskSpace.find(index);
+
+    if (itr == diskSpace.end()) {
+        // TODO: print an error message
+        // this probably shouldn't ever occur
+        return;
+    }
+
+    char display = itr->second.getDisplayChar();
+    int blocks = 0;
+
+    // deallocate all of the blocks
+    while (itr != diskSpace.end()) {
+        DiskCluster tmp (itr->second.getSize(), "", '.');
+        index = itr->second.getNextCluster();
+
+        blocks += itr->second.getSize();
+
+        diskSpace[itr->first] = tmp;
+        itr = diskSpace.find(index);
+    }
+
+    // remove the file mapping as well
+    fileMap.erase(name);
+
+    std::cout << "[thread " << pthread_self() << "] Deleted " << name << " file ";
+    std::cout << "'" << display << "' (deallocated ";
+    if (blocks == 1)
+        std::cout << blocks << " block)" << std::endl;
+    else
+        std::cout << blocks << " blocks)" << std::endl;
+
     printSpace();
 }
 
@@ -73,6 +185,7 @@ DiskCluster::DiskCluster() {
     size = 0;
     name = "";
     displayChar = '?';
+    nextCluster = -1;
 }
 
 // DiskCluster(int, std::string, char) - create a disk cluster with size s, name and char
@@ -80,4 +193,5 @@ DiskCluster::DiskCluster(int s, const std::string& n, char d) {
     size = s;
     name = n;
     displayChar = d;
+    nextCluster = -1;
 }
--- test_disk.cpp
+++ test_disk.cpp
@@ -8,4 +8,15 @@ int main(int argc, char* argv[]) {
     
     // add and remove files, and print the results to test simulation
     d.allocFile("abc.txt", 25842);
+    d.allocFile("def.txt", 86014);
+    d.allocFile("ghi.txt", 32765);
+    d.allocFile("jkl.txt", 36860);
+    d.allocFile("mno.txt", 204795);
+    d.allocFile("pqr.txt", 69629);
+    d.allocFile("stu.txt", 61337);
+
+    d.deallocFile("jkl.txt");
+    d.deallocFile("pqr.txt");
+
+    d.allocFile("jkl.txt", 79112);
 }
